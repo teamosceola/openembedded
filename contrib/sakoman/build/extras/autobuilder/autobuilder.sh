11664f14e45fdc2dfb27eba09a1f705599020749
Steve Sakoman <steve@sakoman.com>
2010-11-05 15:22:33 -0700

    Add contrib/sakoman

---
+++ contrib/sakoman/build/extras/autobuilder/autobuilder.sh
@@ -0,0 +1,222 @@
+#!/bin/bash
+# Copyright Sakoman Inc. 2010-2011
+
+# edit if necessary to reflect your setup
+source $OETOP/build/profile
+
+TMPDIR="/media/data/gnome-r13-tmp"
+RELEASE="gnome-r13"
+BUILD_LIBC="eglibc"
+
+# path to feed directory
+OE_FEED="/var/www/feeds/$RELEASE/ipk/$BUILD_LIBC/armv7a"
+
+#path to store local archive of images
+ARCHIVE="/var/www/feeds/$RELEASE/images"
+
+if [ ! -d $OE_FEED ]; then
+	mkdir -p $OE_FEED
+fi
+
+if [ ! -d $ARCHIVE ]; then
+	mkdir -p $ARCHIVE
+fi
+
+# list of machines to build machine dependent packages for
+MACHINES_MACH=" \
+                overo \
+                beagleboard \
+               "
+# read list of machine dependent targets
+BUILD_TARGETS_MACH=`cat $OETOP/build/extras/autobuilder/targets-mach`
+
+# list of machines to build architecture dependent packages for
+MACHINES_ARCH=" \
+                omap3-multi \
+               "
+# list of architecture dependent targets
+BUILD_TARGETS_ARCH=" \
+                linux-sakoman \
+                sakoman-minimal-image \
+                sakoman-console-image \
+                sakoman-gnome-image \
+                linux-sakoman-initramfs-ro \
+                linux-sakoman-wl1271 \
+               "
+
+cd $OETOP/openembedded
+git checkout $RELEASE && git fetch && git reset --hard origin/$RELEASE
+
+REVISION=`date +%Y%m%d%H%M`
+COMMIT=`git log -1 --pretty=oneline`
+
+BUILD_CLEAN_MACH="x-load-sakoman u-boot-sakoman"
+BUILD_CLEAN_ARCH="linux-sakoman linux-sakoman-initramfs-ro linux-sakoman-wl1271 omap3-sgx-modules task-base task-boot base-files base-passwd sysvinit angstrom-feed-configs angstrom-task-gnome"
+
+if [ -e $OE_FEED ];
+then
+  echo "Building revision $REVISION"
+  
+  mv $OETOP/build/conf/auto.conf $OETOP/build/conf/auto.conf.bak
+
+  #do machine dependent packages first   
+  for machine in $MACHINES_MACH
+  do
+    if [ -e $TMPDIR ]
+    then
+      ANGSTROMLIBC=$BUILD_LIBC MACHINE=$machine bitbake -c clean $BUILD_CLEAN_MACH
+    fi
+
+    for target in $BUILD_TARGETS_MACH
+    do
+      echo "Building $BUILD_LIBC $target for $machine"
+      ANGSTROMLIBC=$BUILD_LIBC MACHINE=$machine bitbake $target
+      echo "Completed $BUILD_LIBC $target for $machine"
+    done
+  done
+
+  #do architecture dependent packages next   
+  for machine in $MACHINES_ARCH
+  do
+    if [ -e $TMPDIR ]
+    then
+      ANGSTROMLIBC=$BUILD_LIBC MACHINE=$machine bitbake -c clean $BUILD_CLEAN_ARCH
+    fi
+
+    for target in $BUILD_TARGETS_ARCH
+    do
+      echo "Building $BUILD_LIBC $target for $machine"
+      ANGSTROMLIBC=$BUILD_LIBC MACHINE=$machine bitbake $target
+      echo "Completed $BUILD_LIBC $target for $machine"
+    done
+  done
+
+  mv $OETOP/build/conf/auto.conf.bak $OETOP/build/conf/auto.conf
+
+# make repo directories
+  mkdir -p $OE_FEED/../all
+  mkdir -p $OE_FEED/base
+  mkdir -p $OE_FEED/debug
+  mkdir -p $OE_FEED/gstreamer
+  mkdir -p $OE_FEED/perl
+  mkdir -p $OE_FEED/python
+  for machine in $MACHINES_MACH $MACHINES_ARCH
+  do
+    mkdir -p $OE_FEED/machine/$machine
+  done
+
+# archive images
+  for machine in $MACHINES_MACH
+  do
+    mkdir -p $ARCHIVE/$machine/$REVISION
+    cp --update $TMPDIR/deploy/$BUILD_LIBC/images/$machine/MLO-$machine		$ARCHIVE/$machine/$REVISION/MLO-$machine-$REVISION
+    cp --update $TMPDIR/deploy/$BUILD_LIBC/images/$machine/u-boot-$machine.bin	$ARCHIVE/$machine/$REVISION/u-boot-$machine-$REVISION.bin
+
+    cd $ARCHIVE/$machine/$REVISION
+    echo $COMMIT > sakoman-oe-commit-id.txt
+    md5sum MLO-$machine-$REVISION > md5sum.txt
+    md5sum u-boot-$machine-$REVISION.bin >> md5sum.txt
+
+    rm -rf $ARCHIVE/$machine/current
+    mkdir -p $ARCHIVE/$machine/current
+    echo $REVISION > $ARCHIVE/$machine/current/current-is-$REVISION
+    ln -s $ARCHIVE/$machine/$REVISION/md5sum.txt			$ARCHIVE/$machine/current/md5sum.txt
+    ln -s $ARCHIVE/$machine/$REVISION/sakoman-oe-commit-id.txt		$ARCHIVE/$machine/current/sakoman-oe-commit-id.txt
+    ln -s $ARCHIVE/$machine/$REVISION/MLO-$machine-$REVISION		$ARCHIVE/$machine/current/MLO
+    ln -s $ARCHIVE/$machine/$REVISION/u-boot-$machine-$REVISION.bin	$ARCHIVE/$machine/current/u-boot.bin
+
+  done
+
+  for machine in $MACHINES_ARCH
+  do
+    mkdir -p $ARCHIVE/$machine/$REVISION
+    cp --update $TMPDIR/deploy/$BUILD_LIBC/images/$machine/uImage-$machine.bin				$ARCHIVE/$machine/$REVISION/uImage-$machine-$REVISION.bin
+    cp --update $TMPDIR/deploy/$BUILD_LIBC/images/$machine/uImage-initramfs-ro-$machine.bin		$ARCHIVE/$machine/$REVISION/uImage-initramfs-ro-$machine-$REVISION.bin
+    cp --update $TMPDIR/deploy/$BUILD_LIBC/images/$machine/uImage-wl1271-$machine.bin			$ARCHIVE/$machine/$REVISION/uImage-wl1271-$machine-$REVISION.bin
+    cp --update $TMPDIR/deploy/$BUILD_LIBC/images/$machine/sakoman-minimal-image-$machine.tar.bz2	$ARCHIVE/$machine/$REVISION/sakoman-minimal-image-$machine-$REVISION.tar.bz2
+    cp --update $TMPDIR/deploy/$BUILD_LIBC/images/$machine/sakoman-console-image-$machine.tar.bz2	$ARCHIVE/$machine/$REVISION/sakoman-console-image-$machine-$REVISION.tar.bz2
+    cp --update $TMPDIR/deploy/$BUILD_LIBC/images/$machine/sakoman-gnome-image-$machine.tar.bz2		$ARCHIVE/$machine/$REVISION/sakoman-gnome-image-$machine-$REVISION.tar.bz2
+    cp --update $TMPDIR/deploy/$BUILD_LIBC/images/$machine/modules-*-*-$machine.tgz			$ARCHIVE/$machine/$REVISION/modules-$machine-$REVISION.tgz
+    cp --update $OETOP/openembedded/contrib/sakoman/mksdcard.sh						$ARCHIVE/$machine/$REVISION/
+
+    cd $ARCHIVE/$machine/$REVISION
+    echo $COMMIT > sakoman-oe-commit-id.txt
+    md5sum uImage-$machine-$REVISION.bin >> md5sum.txt
+    md5sum uImage-initramfs-ro-$machine-$REVISION.bin >> md5sum.txt
+    md5sum uImage-wl1271-$machine-$REVISION.bin >> md5sum.txt
+    md5sum sakoman-console-image-$machine-$REVISION.tar.bz2 >> md5sum.txt
+    md5sum sakoman-minimal-image-$machine-$REVISION.tar.bz2 >> md5sum.txt
+    md5sum sakoman-gnome-image-$machine-$REVISION.tar.bz2 >> md5sum.txt
+    md5sum modules-$machine-$REVISION.tgz >> md5sum.txt
+    md5sum mksdcard.sh >> md5sum.txt
+
+    rm -rf $ARCHIVE/$machine/current
+    mkdir -p $ARCHIVE/$machine/current
+    echo $REVISION > $ARCHIVE/$machine/current/current-is-$REVISION
+    ln -s $ARCHIVE/$machine/$REVISION/md5sum.txt					$ARCHIVE/$machine/current/md5sum.txt
+    ln -s $ARCHIVE/$machine/$REVISION/sakoman-oe-commit-id.txt				$ARCHIVE/$machine/current/sakoman-oe-commit-id.txt
+    ln -s $ARCHIVE/$machine/$REVISION/uImage-$machine-$REVISION.bin			$ARCHIVE/$machine/current/uImage
+    ln -s $ARCHIVE/$machine/$REVISION/uImage-initramfs-ro-$machine-$REVISION.bin	$ARCHIVE/$machine/current/uImage-initramfs-ro
+    ln -s $ARCHIVE/$machine/$REVISION/uImage-wl1271-$machine-$REVISION.bin		$ARCHIVE/$machine/current/uImage-wl1271
+    ln -s $ARCHIVE/$machine/$REVISION/sakoman-minimal-image-$machine-$REVISION.tar.bz2	$ARCHIVE/$machine/current/sakoman-minimal-image.tar.bz2
+    ln -s $ARCHIVE/$machine/$REVISION/sakoman-console-image-$machine-$REVISION.tar.bz2	$ARCHIVE/$machine/current/sakoman-console-image.tar.bz2
+    ln -s $ARCHIVE/$machine/$REVISION/sakoman-gnome-image-$machine-$REVISION.tar.bz2	$ARCHIVE/$machine/current/sakoman-gnome-image.tar.bz2
+    ln -s $ARCHIVE/$machine/$REVISION/modules-$machine-$REVISION.tgz			$ARCHIVE/$machine/current/modules.tgz
+    ln -s $ARCHIVE/$machine/$REVISION/mksdcard.sh					$ARCHIVE/$machine/current/mksdcard.sh
+
+  done
+
+# now we construct a local feed
+# copy package files
+  cp --update $TMPDIR/deploy/$BUILD_LIBC/ipk/all/*    $OE_FEED/../all
+  cp --update $TMPDIR/deploy/$BUILD_LIBC/ipk/armv7a/* $OE_FEED/base
+  for machine in $MACHINES_MACH $MACHINES_ARCH
+  do
+      cp --update $TMPDIR/deploy/$BUILD_LIBC/ipk/$machine/* $OE_FEED/machine/$machine
+  done
+
+# get rid of unneeded files
+  rm -rf $OE_FEED/base/morgue
+
+# place package files in appropriate feed directory
+  mv -u $OE_FEED/../all/*-dbg_*     	$OE_FEED/debug
+  mv -u $OE_FEED/base/*-dbg_*    	$OE_FEED/debug
+  mv -u $OE_FEED/base/gst-*      	$OE_FEED/gstreamer
+  mv -u $OE_FEED/base/gstreamer* 	$OE_FEED/gstreamer
+  mv -u $OE_FEED/base/perl*      	$OE_FEED/perl
+  mv -u $OE_FEED/base/python*    	$OE_FEED/python
+
+# Create Packages.gz for feeds
+  cd $OE_FEED/../all
+  $OETOP/build/extras/autobuilder/ipkg-utils/ipkg-make-index . > Packages
+  gzip -f Packages
+
+  cd $OE_FEED/base
+  $OETOP/build/extras/autobuilder/ipkg-utils/ipkg-make-index . > Packages
+  gzip -f Packages
+
+  for machine in $MACHINES_MACH $MACHINES_ARCH
+  do
+    cd $OE_FEED/machine/$machine
+    $OETOP/build/extras/autobuilder/ipkg-utils/ipkg-make-index . > Packages
+    gzip -f Packages
+  done
+
+  cd $OE_FEED/debug
+  $OETOP/build/extras/autobuilder/ipkg-utils/ipkg-make-index . > Packages
+  gzip -f Packages
+
+  cd $OE_FEED/gstreamer
+  $OETOP/build/extras/autobuilder/ipkg-utils/ipkg-make-index . > Packages
+  gzip -f Packages
+
+  cd $OE_FEED/perl
+  $OETOP/build/extras/autobuilder/ipkg-utils/ipkg-make-index . > Packages
+  gzip -f Packages
+
+  cd $OE_FEED/python
+  $OETOP/build/extras/autobuilder/ipkg-utils/ipkg-make-index . > Packages
+  gzip -f Packages
+
+fi
+
---
+++ contrib/sakoman/mksdcard.sh
@@ -0,0 +1,224 @@
+#! /bin/sh
+# fdisk portion of script based on mkcard.sh v0.4
+# (c) Copyright 2009 Graeme Gregory <dp@xora.org.uk>
+# Additional functionality by Steve Sakoman
+# (c) Copyright 2010-2011 Steve Sakoman <steve@sakoman.com>
+# Licensed under terms of GPLv2
+#
+# Parts of the procudure base on the work of Denys Dmytriyenko
+# http://wiki.omap.com/index.php/MMC_Boot_Format
+
+VERSION=0.7
+RELEASE="gnome-r13"
+
+export LC_ALL=C
+
+FEEDURL="http://feeds.sakoman.com/feeds/$RELEASE/images"
+MOUNT_POINT="/media/sakoman-sd"
+
+PWD=`pwd`
+WORK_DIR=$PWD
+
+if [ $# -eq 0 ]; then
+	echo ""
+	echo "$0 version $VERSION"
+	echo "Usage: $0 <drive> <machine> <image> <release>"
+	echo "   drive: SD device (i.e. /dev/sdc)"
+	echo "   machine: target machine (default overo)"
+	echo "            beagleboard"
+	echo "            omap4430-panda"
+	echo "            overo"
+	echo "   image: image name (default gnome)"
+	echo "            console"
+	echo "            gnome"
+	echo "            minimal"
+	echo "   release: release name (default current)"
+	echo "            current (most recent build)"
+	echo "            build-timestamp (i.e. 201011182053)"
+	echo "            local (use files in current directory)"
+	echo ""
+	exit 1;
+fi
+
+if ! id | grep -q root; then
+	echo "This utility must be run prefixed with sudo or as root"
+	exit 1;
+fi
+
+if [ "$1" = "/dev/sda" ] ; then
+	echo "Sorry, /dev/sda probably holds your PC's rootfs.  Please specify a SD device."
+	exit 1;
+fi
+DRIVE=$1
+
+if [ -z "$2" ]; then
+	MACHINE="overo"
+else
+	MACHINE=$2
+fi
+
+if [ -z "$3" ]; then
+	IMAGE="gnome"
+else
+	IMAGE=$3
+fi
+
+if [ -z "$4" ]; then
+	RELEASE="current"
+else
+	RELEASE=$4
+fi
+
+case $MACHINE in
+	beagleboard )
+		ARCH="omap3-multi" ;;
+	overo )
+		ARCH="omap3-multi" ;;
+	rockhopper )
+		ARCH="omap3-multi" ;;
+	omap4430-panda )
+		ARCH="omap4-multi" ;;
+	* )
+		echo "Error: unkown machine: $MACHINE"
+		exit 1;;
+esac
+
+MACHURL="$FEEDURL/$MACHINE/$RELEASE"
+ARCHURL="$FEEDURL/$ARCH/$RELEASE"
+
+#make sure that the SD card isn't mounted before we start
+if [ -b ${DRIVE}1 ]; then
+	umount ${DRIVE}1
+	umount ${DRIVE}2
+elif [ -b ${DRIVE}p1 ]; then
+	umount ${DRIVE}p1
+	umount ${DRIVE}p2
+else
+	umount ${DRIVE}
+fi
+
+dd if=/dev/zero of=$DRIVE bs=1024 count=1024
+
+SIZE=`fdisk -l $DRIVE | grep Disk | grep bytes | awk '{print $5}'`
+
+echo DISK SIZE – $SIZE bytes
+
+CYLINDERS=`echo $SIZE/255/63/512 | bc`
+
+echo CYLINDERS – $CYLINDERS
+
+{
+echo ,9,0x0C,*
+echo ,,,-
+} | sfdisk -D -H 255 -S 63 -C $CYLINDERS $DRIVE
+
+sleep 1
+
+if [ -b ${DRIVE}1 ]; then
+	PART1=${DRIVE}1
+	PART2=${DRIVE}2
+elif [ -b ${DRIVE}p1 ]; then
+	PART1=${DRIVE}p1
+	PART2=${DRIVE}p2
+else
+	echo "Improper partitioning on $DRIVE"
+	exit 1;
+fi
+
+umount ${PART1}
+mkfs.vfat -F 32 -n boot ${PART1}
+
+umount ${PART2}
+mke2fs -j -L rootfs ${PART2}
+
+if [ "$RELEASE" = "local" ]; then
+	echo "Using local files"
+else
+	echo "Backing up previously downloaded files (if any)"
+	mv -b MLO MLO.bak
+	mv -b u-boot.bin u-boot.bin.bak
+	mv -b uImage uImage.bak
+	mv -b sakoman-$IMAGE-image.tar.bz2 sakoman-$IMAGE-image.tar.bz2.bak
+
+	echo "Downloading files from $MACHURL"
+	if [ "$RELEASE" = "current" ]; then	
+		wget $MACHURL/MLO
+		wget $MACHURL/u-boot.bin
+		wget $ARCHURL/uImage
+		wget $ARCHURL/sakoman-$IMAGE-image.tar.bz2
+	else
+		if wget $MACHURL/MLO-$MACHINE-$RELEASE; then
+			mv   MLO-$MACHINE-$RELEASE MLO
+		fi
+     
+		if wget $MACHURL/u-boot-$MACHINE-$RELEASE.bin; then
+			mv   u-boot-$MACHINE-$RELEASE.bin u-boot.bin
+		fi
+
+		if wget $ARCHURL/uImage-$ARCH-$RELEASE.bin; then
+			mv   uImage-$ARCH-$RELEASE.bin uImage
+		fi
+
+		if wget $ARCHURL/sakoman-$IMAGE-image-$ARCH-$RELEASE.tar.bz2; then
+			mv   sakoman-$IMAGE-image-$ARCH-$RELEASE.tar.bz2 sakoman-$IMAGE-image.tar.bz2
+		fi
+	fi
+fi
+
+# create a mount point if it doesn't already exist
+if [ ! -d $MOUNT_POINT ]; then
+	if ! mkdir $MOUNT_POINT; then
+		echo "Could not create mount point: $MOUNT_POINT"
+		echo "SD card creation was not successful"
+		exit 1;
+	fi
+fi
+
+if mount -t vfat ${PART1} $MOUNT_POINT; then
+	echo "Populating boot partition files"
+	cp MLO        $MOUNT_POINT
+	cp u-boot.bin $MOUNT_POINT
+	cp uImage     $MOUNT_POINT
+	sync
+	umount $MOUNT_POINT
+	echo "Boot partition complete"
+else
+	echo "Can't mount ${PART1} at $MOUNT_POINT for boot partition creation"
+	echo "SD card creation was not successful"
+	exit 1;
+fi
+
+# this shouldn't be necessary, but Ubuntu seems to remove the mount point!
+if [ ! -d $MOUNT_POINT ]; then
+	if ! mkdir $MOUNT_POINT; then
+		echo "Could not create mount point: $MOUNT_POINT"
+		echo "SD card creation was not successful"
+		exit 1;
+	fi
+fi
+
+if mount -t ext3 ${PART2} $MOUNT_POINT; then
+	if cd $MOUNT_POINT; then
+		echo "Populating rootfs partition files"
+		echo "This will take several minutes . . ."
+		tar xf $WORK_DIR/sakoman-$IMAGE-image.tar.bz2
+		sync
+		cd $WORK_DIR
+		umount $MOUNT_POINT
+		echo "Rootfs partition complete"
+	else
+		echo "Error populating rootfs partition"
+		echo "SD card creation was not successful"
+	fi
+else
+	echo "Can't mount ${PART2} at $MOUNT_POINT for rootfs creation"
+	echo "SD card creation was not successful"
+	exit 1;
+	
+fi
+
+echo "SD card creation was successful"
+
+exit 0;
+
+
