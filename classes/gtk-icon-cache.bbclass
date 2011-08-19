teamosceola/openembedded

commit 0b521d7bcfbb6f26bb2d3ad55add6a3f677a199d

gtk-icon-cache bbclass: only add runtime dependencies on hicolor-icon-theme when installing icons
Also import a fix from OE-core for setting the loader dir.

Tested with gnome-icon-theme and libsoup recipes on angstrom.

Signed-off-by: Koen Kooi <koen@openembedded.org>
Acked-by: Steffen Sledz <sledz@dresearch-fe.de>

classes/gtk-icon-cache.bbclass
@@ -1,6 +1,4 @@
 FILES_${PN} += "${datadir}/icons/hicolor"
-# Only add runtime dependency on hicolor-icon-theme for target recipes
-RDEPENDS_${PN} += "${@['', 'hicolor-icon-theme']['${PN}' == '${BPN}']}"
 
 # This could run on the host as icon cache files are architecture independent,
 # but there is no gtk-update-icon-cache built natively.
@@ -10,7 +8,7 @@ if [ "x$D" != "x" ]; then
 fi
 
 # Update the pixbuf loaders in case they haven't been registered yet
-gdk-pixbuf-query-loaders > /etc/gtk-2.0/gdk-pixbuf.loaders
+GDK_PIXBUF_MODULEDIR=${libdir}/gdk-pixbuf-2.0/2.10.0/loaders gdk-pixbuf-query-loaders --update-cache
 
 for icondir in /usr/share/icons/* ; do
     if [ -d $icondir ] ; then
@@ -35,7 +33,12 @@ python populate_packages_append () {
 		icon_dir = '%s/%s/%s/icons' % (pkgdest, pkg, bb.data.getVar('datadir', d, 1))
 		if not os.path.exists(icon_dir):
 			continue
-		
+
+		bb.note("adding hicolor-icon-theme dependency to %s" % pkg)	
+		rdepends = bb.data.getVar('RDEPENDS', d, 1)
+		rdepends += "hicolor-icon-theme"
+		bb.data.setVar('RDEPENDS', rdepends, d)
+	
 		bb.note("adding gtk-icon-cache postinst and postrm scripts to %s" % pkg)
 		
 		postinst = bb.data.getVar('pkg_postinst_%s' % pkg, d, 1) or bb.data.getVar('pkg_postinst', d, 1)

commit 6edddf5a6638c2ee2f7f09146cda734d862d4ecd

gtk-icon-cache.bbclass: Fixes bug introduced by commit 0b521d7bcfbb6f26bb2d3ad55add6a3f677a199d
Add back creation of /etc/gtk-2.0/gdk-pixbuf.loaders
Add missing space to rdepends append

Signed-off-by: Steve Sakoman <steve@sakoman.com>

Fixes bug introduced by commit 0b521d7bcfbb6f26bb2d3ad55add6a3f677a199d

classes/gtk-icon-cache.bbclass
@@ -8,7 +8,7 @@ if [ "x$D" != "x" ]; then
 fi
 
 # Update the pixbuf loaders in case they haven't been registered yet
-GDK_PIXBUF_MODULEDIR=${libdir}/gdk-pixbuf-2.0/2.10.0/loaders gdk-pixbuf-query-loaders --update-cache
+gdk-pixbuf-query-loaders > /etc/gtk-2.0/gdk-pixbuf.loaders
 
 for icondir in /usr/share/icons/* ; do
     if [ -d $icondir ] ; then
@@ -36,7 +36,7 @@ python populate_packages_append () {
 
 		bb.note("adding hicolor-icon-theme dependency to %s" % pkg)	
 		rdepends = bb.data.getVar('RDEPENDS', d, 1)
-		rdepends += "hicolor-icon-theme"
+		rdepends += "hicolor-icon-theme "
 		bb.data.setVar('RDEPENDS', rdepends, d)
 	
 		bb.note("adding gtk-icon-cache postinst and postrm scripts to %s" % pkg)